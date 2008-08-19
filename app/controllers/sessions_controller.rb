e4eac1923db7a9ff9d8900974fef65526313dbc2
Brooke Riggio <brookr@pridwen.local>
2008-08-19 16:37:59 -0700

    initial load of SaaS Rails Kit

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,27 @@
+# This controller handles the login/logout function of the site.  
+class SessionsController < ApplicationController
+  skip_before_filter :login_required, :except => :destroy
+
+  def create
+    self.current_user = current_account.users.authenticate(params[:login], params[:password])
+    if logged_in?
+      if params[:remember_me] == "1"
+        self.current_user.remember_me
+        cookies[:auth_token] = { :value => self.current_user.remember_token , :expires => self.current_user.remember_token_expires_at }
+      end
+      redirect_back_or_default('/')
+      flash[:notice] = "Logged in successfully"
+    else
+      flash.now[:error] = 'Invalid login credentials'
+      render :action => 'new'
+    end
+  end
+
+  def destroy
+    self.current_user.forget_me if logged_in?
+    cookies.delete :auth_token
+    reset_session
+    flash[:notice] = "You have been logged out."
+    redirect_back_or_default('/')
+  end
+end
