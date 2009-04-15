470ef5b0eb4690b104887d68b6634329e003ab53
Jack Danger Canty <gitcommit@6brand.com>
2009-04-14 17:08:50 -0700

    filling out should_eventuallies for event test

--- test/unit/event_test.rb
+++ test/unit/event_test.rb
@@ -19,8 +19,83 @@
 require File.dirname(__FILE__) + '/../test_helper'
 
 class EventTest < ActiveSupport::TestCase
-  should "be valid with factory" do
-    assert_valid Factory.build(:event)
+
+  should_require_attributes :name
+  should_require_attributes :creator_id
+
+  context "creator" do
+    setup { @event = Factory(:event) }
+    should_eventually "be a user" do
+    end
   end
- 
+  
+  context "owner" do
+    setup { @event = Factory(:event) }
+    should_eventually "be a user" do
+    end
+  end
+
+  context "Appointment" do
+    setup do
+      @event = Factory(:appointment)
+    end
+    should_eventually "have start date" do
+    end
+    should_eventually "have end date" do
+    end
+    should_eventually "have start time" do
+    end
+    should_eventually "have end time" do
+    end
+  end
+
+  context "Deadline" do
+    setup do
+      @event = Factory(:deadline)
+    end
+    should_eventually "have start date" do
+    end
+    should_eventually "have no end date" do
+    end
+    should_eventually "have start time" do
+    end
+    should_eventually "have no end time" do
+    end
+    should_eventually "be completable" do
+    end
+  end
+  
+  context "Task" do
+    setup do
+      @event = Factory(:task)
+    end
+    should_eventually "have a start date" do
+    end
+    should_eventually "have no end date" do
+    end
+    should_eventually "have no start time" do
+    end
+    should_eventually "have no end time" do
+    end
+    should_eventually "be completable" do
+    end
+  end
+  
+  context "AllDay" do
+    setup do
+      @event = Factory(:task)
+    end
+    should_eventually "have a start date" do
+    end
+    should_eventually "have an end date" do
+    end
+    should_eventually "have no start time" do
+    end
+    should_eventually "have no end time" do
+    end
+    should_eventually "be able to be only one day" do
+    end
+    should_eventually "be able to span several days" do
+    end
+  end
 end
