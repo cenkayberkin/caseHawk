43a8f595f93ddf8892f8e1d65794446275b6908b
carlosdavis <carlos@haplo.local>
2009-04-21 16:19:43 -0700

    Rebuilt the application layout and started on new CSS scheme

--- config/initializers/time_formats.rb
+++ config/initializers/time_formats.rb
@@ -4,5 +4,6 @@ ActiveSupport::CoreExtensions::Time::Conversions::DATE_FORMATS.merge!(
   :us => '%m/%d/%y',
   :us_with_time => '%m/%d/%y, %l:%M %p',
   :short_day => '%e %B %Y',
-  :long_day => '%A, %e %B %Y'
+  :long_day => '%A, %e %B %Y',
+  :year => '%Y'
 )
