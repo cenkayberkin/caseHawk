327ec6f3f8a0866a885850a4cd329055671c8a10
Jack Danger Canty <gitcommit@6brand.com>
2009-04-23 22:10:42 -0700

    account factory finally working.

--- test/unit/account_test.rb
+++ test/unit/account_test.rb
@@ -17,6 +17,11 @@ include ActiveMerchant::Billing
 
 class AccountTest < ActiveSupport::TestCase
 
+  should "be valid with factory" do
+    Factory(:account)
+    assert Factory.build(:account).save!, Factory.build(:account).errors.inspect
+  end
+
   context "Account" do
     setup do
       @account = accounts(:localhost)
