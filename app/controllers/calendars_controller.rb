1416b0847bf37f61922898f996ceb25f5330c934
Jack Danger Canty <gitcommit@6brand.com>
2009-05-12 13:45:32 -0700

    rendering all calendars as json atm.

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -1,20 +1,10 @@
 class CalendarsController < ApplicationController
 
   def index
+    render :json => Event.all.to_json
   end
 
-  def show
-    render :text =>  RiCal.Calendar do |cal|
-      cal.event do |event|
-        event.description = "MA-6 First US Manned Spaceflight"
-        event.dtstart =  DateTime.parse("2/20/1962 14:47:39")
-        event.dtend = DateTime.parse("2/20/1962 19:43:02")
-        event.location = "Cape Canaveral"
-        event.add_attendee "john.glenn@nasa.gov"
-        event.alarm do
-          description "Segment 51"
-        end
-      end
-    end
+  def today
+    render :json => Event.today.to_json
   end
 end
