00ded0437ce71d625847fe1152d8be156cb05bc4
Jack Danger Canty <gitcommit@6brand.com>
2009-05-12 15:16:32 -0700

    annotating models

--- test/unit/task_test.rb
+++ test/unit/task_test.rb
@@ -1,3 +1,22 @@
+# == Schema Information
+#
+# Table name: events
+#
+#  id          :integer(4)      not null, primary key
+#  creator_id  :integer(4)      not null
+#  owner_id    :integer(4)
+#  location_id :integer(4)
+#  type        :string(255)     not null
+#  name        :string(255)     not null
+#  start_date  :date
+#  start_time  :time
+#  end_date    :date
+#  end_time    :time
+#  remind      :boolean(1)
+#  created_at  :datetime
+#  updated_at  :datetime
+#
+
 require File.dirname(__FILE__) + '/../test_helper'
 
 class TaskTest < ActiveSupport::TestCase
