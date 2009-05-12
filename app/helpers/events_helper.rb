6d586c9e929e0928568120c16ea287e39f3476f4
Brooke Riggio <brookr@pridwen.local>
2009-05-12 15:23:31 -0700

    Added events helper for filtering array of events

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -1,2 +1,24 @@
 module EventsHelper
+  
+  # Use these methods to get events of a certain type: get_tasks(@events) 
+  
+  def get_type(records, type)
+    records.select { |r| r.class == type }
+  end
+  
+  def get_tasks(records)
+    get_type(records, Task)
+  end
+  
+  def get_appointments(records)
+    get_type(records, Appointment)
+  end
+  
+  def get_deadlines(records)
+    get_type(records, Deadline)
+  end
+  
+  def get_all_days(records)
+    get_type(records, AllDay)
+  end
 end
