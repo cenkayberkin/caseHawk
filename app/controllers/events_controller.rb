5c1f202dd2af6a851a2cdc4e08c918f0c640c270
Jack Danger Canty <gitcommit@6brand.com>
2009-05-12 12:01:46 -0700

    subclassing Event in controller

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,39 +1,52 @@
 class EventsController < ApplicationController
 
-  def index
-  end
+  before_filter :find_or_initialize
+
+  # render :index
 
   def new
     @event = Event.new
   end
 
   def create
-    @event = Event.new(params[:event])
-    @event.save
+    @event.update_attributes(params[:event])
     flash[:success] = 'Event created.'
     redirect_to events_path
   end
 
-  def show
-    @event = Event.find(params[:id])
-  end
+  # render :show
 
-  def edit
-    @event = Event.find(params[:id])
-  end
+  # render :edit
 
   def update
-    @event = Event.find(params[:id])
     @event.update_attributes(params[:event])
     flash[:success] = 'Event updated.'
     redirect_to events_path
   end
 
   def destroy
-    @event = Event.find(params[:id])
     @event.destroy
     flash[:success] = 'Event deleted.'
     redirect_to events_path
   end
+
+  protected
+
+    def new_event(atts = {})
+      case params[:event] && params[:event][:event_type]
+      when 'AllDay'       then AllDay.new(atts)
+      when 'Appointment'  then Appointment.new(atts)
+      when 'Deadline'     then Deadline.new(atts)
+      when 'Task'         then Task.new(atts)
+      else
+        Event.new(atts)
+      end
+    end
+
+    def find_or_initialize
+      @event = params[:id] ?
+                 Event.find(params[:id]) :
+                 new_event(params[:event])
+    end
 
 end
