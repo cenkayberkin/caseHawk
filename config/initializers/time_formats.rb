0a6a646c4bb3354a44d1990038a05bcb4bd03c43
Brooke Riggio <brookr@pridwen.local>
2009-05-12 17:10:23 -0700

    added simple time format

--- config/initializers/time_formats.rb
+++ config/initializers/time_formats.rb
@@ -5,5 +5,6 @@ ActiveSupport::CoreExtensions::Time::Conversions::DATE_FORMATS.merge!(
   :us_with_time => '%m/%d/%y, %l:%M %p',
   :short_day => '%e %B %Y',
   :long_day => '%A, %e %B %Y',
-  :year => '%Y'
+  :year => '%Y',
+  :simple => '%l:%M%P'
 )
