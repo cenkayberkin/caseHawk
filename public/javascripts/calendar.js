5c6875e73b9e4c581b5dfc41759dd2ae2e9a32b6
Jack Danger Canty <gitcommit@6brand.com>
2009-05-15 13:31:49 -0700

    creating calendar js api

--- public/javascripts/calendar.js
+++ public/javascripts/calendar.js
@@ -1,17 +1,48 @@
+// 
+// Calendar = {tags: ''}
+// 
+// $.extend(Calendar, {
+//   url: function(){
+//     this.tags == '' ?
+//       '/calendar/' :
+//       '/tags/'+this.tags+'/calendar'
+//   },
+//   retrieve: function(weeks_ago){
+//     $.get(this.url(),
+//           {weeks_ago: weeks_ago || 0},
+//           function(content){
+//             console.log(content)
+//           })
+//   }
+// })
+// 
 
-Calendar = {tags: ''}
+// Interface:
+//
+//  Calendar.drawAlllWeeks # draws the top five weeks
+//  Calendar.drawWeek(4)
+//    # makes sure the 5th week from the top is available
+//    # calls:
+//    Calendar.loadWeekEvents(4)
+//      # retrieves and instantiates all events for the 5th week from top
+//  Calendar.drawDay('2009-05-01')
+//    # loads and places all events on the given day
+//    # calls:
+//    Calendar.loadDayEvents('2009-05-01')
+//      # retrieves and instantiates all events for the given day
+//    Calendar.loadDayEvents('2009-05-01', {tags: ["one", "two"]})
+//      # limited to tags
+//    Calendar.placeDayEvents('2009-05-01')
+//      # calculates positions for each event in the current day calendar
+//      # adds height according to time length
+//    Calendar.boxDayEvents('2009-05-01')
+//      # finds intersecting events and groups them into a single event list
 
-$.extend(Calendar, {
-  url: function(){
-    this.tags == '' ?
-      '/calendar/' :
-      '/tags/'+this.tags+'/calendar'
-  },
-  retrieve: function(weeks_ago){
-    $.get(this.url(),
-          {weeks_ago: weeks_ago || 0},
-          function(content){
-            console.log(content)
-          })
-  }
-})
+Calendar = {
+  drawAllWeeks: function(){},
+  drawWeek: function(){},
+  drawDay: function(){},
+  loadDayEvents: function(){},
+  placeDayEvents: function(){},
+  boxDayEvents: function(){}
+}
