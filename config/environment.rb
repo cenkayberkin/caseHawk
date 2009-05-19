2d89d42b4c78cf3824840fde9511053a610a3d6d
Jack Danger Canty <gitcommit@6brand.com>
2009-05-18 23:51:32 -0700

    adding ryan bates' scope_builder

--- config/environment.rb
+++ config/environment.rb
@@ -45,6 +45,7 @@ Rails::Initializer.run do |config|
   }
 
   config.gem "rubyredrick-ri_cal", :lib => 'ri_cal', :source => 'http://gems.github.com'
+  config.gem 'ryanb-scope-builder', :lib => 'scope_builder', :source => 'http://gems.github.com'
 
   # Use the database for sessions instead of the cookie-based default,
   # which shouldn't be used to store highly confidential information
