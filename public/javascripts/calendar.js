027bd86d697ae57cbbf770eacf70fedebb1423a0
Jack Danger Canty <gitcommit@6brand.com>
2009-05-19 00:46:54 -0700

    adapting the api to be callback-centered

--- public/javascripts/calendar.js
+++ public/javascripts/calendar.js
@@ -3,16 +3,11 @@
 //  Calendar.drawAlllWeeks # draws the top five weeks
 //  Calendar.drawWeek(4)
 //    # makes sure the 5th week from the top is available
-//    # calls:
-//    Calendar.loadWeekEvents(4)
-//      # retrieves and instantiates all events for the 5th week from top
+//    # calls via callback:
+//    Calendar.placeWeekEvent
 //  Calendar.drawDay('2009-05-01')
 //    # loads and places all events on the given day
-//    # calls:
-//    Calendar.loadDayEvents('2009-05-01')
-//      # retrieves and instantiates all events for the given day
-//    Calendar.loadDayEvents('2009-05-01', {tags: ["one", "two"]})
-//      # limited to tags
+//    # calls via callback:
 //    Calendar.placeDayEvents('2009-05-01')
 //      # calculates positions for each event in the current day calendar
 //      # adds height according to time length
@@ -20,17 +15,30 @@
 //      # finds intersecting events and groups them into a single event list
 
 Calendar = {
-  drawAllWeeks: function(){},
-  drawWeek: function(){},
-  loadWeekEvents: function(week){
-    return Event.find({week: week})
+  drawAllWeeks: function(){
+    for(i=0;i<5;i++)
+      drawWeek(i)
   },
-  drawDay: function(){},
-  loadDayEvents: function(date, options){
-    return Event.find(
-      $.extend({start_date: date, end_date: date}, options)
+  drawWeek: function(week){
+    Event.find({week: week}, function(,event){
+      placeWeekEvent(event)
+    })
+  },
+  placeWeekEvent: function(event){
+    // draw the event on the page
+  }
+  drawDay: function(date, options){
+    Event.find(
+      $.extend({start_date: date, end_date: date}, options),
+      function(event){
+        placeDayEvent(event)
+      }
     }
   },
-  placeDayEvents: function(){},
-  boxDayEvents: function(){},
+  placeDayEvent: function(){
+    // draw the event on the page
+  },
+  boxDayEvents: function(){
+    // group adjacent events into boxes
+  },
 }
