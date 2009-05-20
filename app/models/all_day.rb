446ef2ad101b65b479e232e3b489626aa4e5bac3
Jack Danger Canty <gitcommit@6brand.com>
2009-05-19 21:03:30 -0700

    Adding Event#starts_at and Event#ends_at datetimes
    These two fields hold all the information previously held in four.
    [ annotating models ]

--- app/models/all_day.rb
+++ app/models/all_day.rb
@@ -2,19 +2,22 @@
 #
 # Table name: events
 #
-#  id          :integer(4)      not null, primary key
-#  creator_id  :integer(4)      not null
-#  owner_id    :integer(4)
-#  location_id :integer(4)
-#  type        :string(255)     not null
-#  name        :string(255)     not null
-#  start_date  :date
-#  start_time  :time
-#  end_date    :date
-#  end_time    :time
-#  remind      :boolean(1)
-#  created_at  :datetime
-#  updated_at  :datetime
+#  id           :integer(4)      not null, primary key
+#  creator_id   :integer(4)      not null
+#  owner_id     :integer(4)
+#  location_id  :integer(4)
+#  type         :string(255)     not null
+#  name         :string(255)     not null
+#  start_date   :date
+#  start_time   :time
+#  end_date     :date
+#  end_time     :time
+#  remind       :boolean(1)
+#  created_at   :datetime
+#  updated_at   :datetime
+#  completed_at :datetime
+#  starts_at    :datetime
+#  ends_at      :datetime
 #
 
 class AllDay < Event
