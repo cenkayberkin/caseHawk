1d7df429a5ec80fe4521c5ce18f4affaad5042a7
Jack Danger Canty <gitcommit@6brand.com>
2009-05-26 12:56:19 -0700

    adding comments to application.js helpers

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,4 +1,5 @@
 
+// use log(whatever) in any script
 log = function(){
   console.log.apply(console,arguments)
 }
@@ -6,6 +7,7 @@ log = function(){
 /* allow jQuery to work with Rails' respond_to */
 $.ajaxSetup({'beforeSend': function(xhr) {xhr.setRequestHeader("Accept", "text/javascript")} })
 
+// "SomeString".underscore() => "some_string"
 String.prototype.underscore = function(){
   var under = [];
   for each(var word in this.split(/([A-Z][a-z]*)/))
@@ -14,6 +16,7 @@ String.prototype.underscore = function(){
   return under.join("_")
 }
 
+// "some_string".camelcase() => "SomeString"
 String.prototype.camelcase = function(){
   var parts = this.split(/[ _-]+/), len = parts.length;
   if (len == 1) return parts[0];
@@ -24,5 +27,3 @@ String.prototype.camelcase = function(){
 
   return camelized;
 }
-
-
