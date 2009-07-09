7f348f444b99e62773ec7e202ff96d532e4af6a7
Jack Danger Canty <gitcommit@6brand.com>
2009-07-09 16:32:46 -0700

    setting Event#completed_by in controller

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -29,7 +29,11 @@ class EventsController < ApplicationController
   end
 
   def update
-    @saved = @event.update_attributes(params[:event])
+    @event.attributes = params[:event]
+    if params[:event] && !params[:event][:completed].blank?
+      @event.completed_by = current_user
+    end
+    @saved = @event.save
     respond_to do |format|
       format.html {
         @saved ?
@@ -38,8 +42,7 @@ class EventsController < ApplicationController
         redirect_to_back_or calendar_path(:date => @event.starts_at.to_date.to_s)
       }
       format.js {
-        render :json => {:record => @event,
-                         :errors => @event.errors }
+        render :json => @event
       }
     end
   end
