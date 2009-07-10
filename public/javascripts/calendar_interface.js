72359b2fa8cd7244964fcadd9c06101c47c1bbb1
Jack Danger Canty <gitcommit@6brand.com>
2009-07-09 17:10:00 -0700

    unified approach to making event-details show up

--- public/javascripts/calendar_interface.js
+++ public/javascripts/calendar_interface.js
@@ -5,10 +5,10 @@ $(function(){
   // Slide in/out of view the event's details
   // when the user clicks an event
   $(".event-details").hide()
-  $(".event-title label")
+  $(".event-title")
     .addClass("clickable")
     .click(function(){
-      var detail = $(this).parents("li").find(".event-details")
+      var detail = $(this).next(".event-details")
       // hide any (other) open detail elements
       $(".event-details").filter(function(){
         return this != detail[0]
