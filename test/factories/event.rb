d710b086604deace8634e06f24c28f4ef6cd3879
Jack Danger Canty <gitcommit@6brand.com>
2009-07-21 09:28:35 -0700

    DRYing up factories

--- test/factories/event.rb
+++ test/factories/event.rb
@@ -1,37 +1,27 @@
-Factory.define :event, :class => 'AllDay' do |e|
+Factory.define :event, :class => 'Appointment' do |e|
   e.creator { User.first || Factory.create(:user) }
   e.name "Meeting with staff at the office"
   e.starts_at "2009-04-15 13:30"
   e.ends_at   "2009-04-15 15:00"
+  e.account { Account.first || Factory.create(:account) }
   e.remind 0
 end
 
-Factory.define :all_day do |e|
-  e.creator { User.first || Factory.create(:user) }
+Factory.define :all_day, :parent => :event, :class => 'AllDay' do |e|
   e.name "Big Tuna Birthday"
   e.starts_at "2009-04-15 00:00"
   e.ends_at   "2009-04-15 00:00"
-  e.remind 0
 end
 
-Factory.define :appointment do |e|
-  e.creator { User.first || Factory.create(:user) }
-  e.name "Meeting with staff at the office"
-  e.starts_at "2009-04-15 13:30"
-  e.ends_at   "2009-04-15 15:00"
-  e.remind 0
+Factory.define :appointment, :parent => :event do
 end
 
-Factory.define :deadline do |e|
-  e.creator { User.first || Factory.create(:user) }
+Factory.define :deadline, :parent => :event, :class => 'Deadline' do |e|
   e.name "Turn it in!"
   e.starts_at "2009-04-15 13:30"
-  e.remind 0
 end
 
-Factory.define :task do |e|
-  e.creator { User.first || Factory.create(:user) }
-  e.name "Things to do"
+Factory.define :task, :parent => :event, :class => 'Task' do |e|
+  e.name "Clean the office"
   e.starts_at "2009-04-15 00:00"
-  e.remind 0
 end
