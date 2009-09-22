2b900a8fc49f677a9c4cb2c0884b7de51d713ab4
Jack Danger Canty <gitcommit@6brand.com>
2009-09-22 13:34:57 -0700

    filling out weeks controller. tests pass

---
+++ app/controllers/weeks_controller.rb
@@ -0,0 +1,22 @@
+class WeeksController < ApplicationController
+
+  before_filter :find_date
+
+  def index
+    redirect_to :action => :show, :id => Date.today.to_s
+  end
+
+  def show
+    @events = events.ordered.week_of(@date).find(:all, :include => :creator)
+    render :action => 'show', :layout => false
+  end
+
+  protected
+
+  def find_date
+    param = params[:id] || params[:date]
+    @date = (param ?
+      Date.parse(param) : Date.today).beginning_of_week.to_date
+  end
+
+end
