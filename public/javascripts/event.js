0c5580eeed957f49d4adf7a680224fba262a8664
Jack Danger Canty <gitcommit@6brand.com>
2009-11-17 15:59:44 -0800

    removing comments from Event.js

--- public/javascripts/event.js
+++ public/javascripts/event.js
@@ -1,25 +1,3 @@
-// Interface:
-// 
-// Event.find(4)
-//   # returns a single event object
-// Event.find({tags: ["one", "two"]})
-//   # returns all events matching tags
-//   # defaults to timeframe of one month past and one month future
-// Event.find({start: '2009-03-04', end: '2009-08-09'})
-//   # returns all events in the given timeframe
-// Event.find({week: 2})
-//   # returns all events of the week 2-weeks before this one
-//   # (so, the third week on the list, starting with '0')
-// Event.find({tags: ["one", "two"],
-//             start: '2009-03-04',
-//             end: '2009-08-09'})
-//   # returns events matching all given criteria
-//   # Event.find calls:
-//   Event.retrieve(arguments)
-//     # which calls via callback:
-//     Event.instantiate(events)
-//       # adds helpful event methods to the json object's prototype
-
 Event = {
   find: function(options, callback){
     if (Number == options.constructor)
