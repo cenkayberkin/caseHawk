525c08caec61191fbe6d76b981af624aa390a862
Jack Danger Canty <gitcommit@6brand.com>
2009-11-24 15:52:13 -0800

    removing obsolete day-view setup

--- public/javascripts/calendar.js
+++ public/javascripts/calendar.js
@@ -1,7 +1,5 @@
 
 $(function(){
-  // if there's a #day then this is a single-day page
-  $("#day").each(Day.init)
   // load the first week
   Week.loadFirst()
   // initialize each week on the page
