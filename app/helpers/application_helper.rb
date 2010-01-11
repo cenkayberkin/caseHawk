1045ec03dd27d7b4f3696ac0cfd98446d5bd0b96
carlosdavis <carlos@nightmoose.local>
2010-01-11 08:20:28 -0600

    Different position, markup and style for notices and errors

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
   def flash_notices
-    [:notice, :error].collect {|type| content_tag('div', flash[type], :id => type) if flash[type] }
+    [:notice, :error].collect {|type| content_tag('div', flash[type], :class => type) if flash[type] }
   end
   
   # Render a submit button and cancel link
