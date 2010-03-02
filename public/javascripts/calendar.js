264b3086ccb939f3dfeafdbfda63c90f86714704
Jack Danger Canty <gitcommit@6brand.com>
2010-03-02 15:09:24 -0800

    extracting loadAgenda into Calendar

--- public/javascripts/calendar.js
+++ public/javascripts/calendar.js
@@ -6,4 +6,34 @@ $(function(){
   $("table.week-events").each(Week.init)
 })
 
-Calendar = {}
+Calendar = {
+  loadAgenda: function(tag){
+    $.getJSON(
+      "/events/",
+      {tags: tag},
+      function(results){
+
+        var list = $("#cal_search_results")
+
+        if(!results.length){
+          list.html("<h3>No results found for <em>"+tag+"</em></h3>")
+          return
+        }
+
+        list
+          .html("<ul class='results'></ul>")
+          .show()
+
+        $.each(results, function(_,result){
+
+          var event = Event.instantiate($(result)[0], 'skip_cache')
+
+          list.append(event)
+
+          $("table.week li.event#"+event.id)
+            .addClass('result')
+        }
+      )
+    })
+  }
+}
