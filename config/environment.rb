6c4634bb3469033ab70684fc70a84d10bb4f257a
Jack Danger Canty <gitcommit@6brand.com>
2010-03-02 13:23:37 -0800

    Revert "adding thoughtbot's pacecar gem."
    This reverts commit 7aec806f48d461b34a4ffe58814d767834a760d2.

--- config/environment.rb
+++ config/environment.rb
@@ -45,7 +45,6 @@ Rails::Initializer.run do |config|
   }
 
   config.gem "rubyredrick-ri_cal", :lib => 'ri_cal', :source => 'http://gems.github.com'
-  config.gem "pacecar"
   config.gem 'ryanb-scope-builder', :lib => 'scope_builder', :source => 'http://gems.github.com'
   config.gem 'mojombo-chronic', :lib => 'chronic', :source => 'http://gems.github.com'
   
