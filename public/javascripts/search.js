b03eacdc5320e1cc8053973eacf59b267c5c36dd
Brooke Riggio <brookr@pridwen-2.local>
2010-03-09 17:01:45 -0800

    Block the event search form from reloading the page on <enter>

--- public/javascripts/search.js
+++ public/javascripts/search.js
@@ -1,18 +1,22 @@
 $(function(){
   
+  $('#search_tag').submit(function() {
+    return false
+  })
+  
   $('#cal_search #event_tag_search')
     .autocomplete("/tags?by_taggable_type=Event", {
       matchContains: true,
       autoFill: false,
       minChars: 0
     })
-    .result(function(_,_,selectedValue){
+    .result(function(_,_,selectedValue) {
       Calendar.loadAgenda(selectedValue)
-      $('#mini_cal table').slideUp(); 
-      $('#new_event').children(":gt(0)").slideUp(); 
+      $('#mini_cal table').slideUp();
+      $('#new_event').children(":gt(0)").slideUp();
     })
 
-    $('#mini_cal').click(function(){
+    $('#mini_cal').click(function() {
       $('#mini_cal table').show()
     })
     $('#new_event input').focus(function() {
