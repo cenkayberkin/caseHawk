7b141e9cc24e7042439a1b7d40dc0215703b61b6
Jack Danger Canty <gitcommit@6brand.com>
2010-06-15 12:30:24 -0700

    adding modified_by_id to version table as well

--- db/migrate/20100615190947_add_modified_by_id_to_events.rb
+++ db/migrate/20100615190947_add_modified_by_id_to_events.rb
@@ -1,9 +1,11 @@
 class AddModifiedByIdToEvents < ActiveRecord::Migration
   def self.up
     add_column :events, :modified_by, :integer
+    add_column :event_versions, :modified_by, :integer
   end
 
   def self.down
+    remove_column :event_versions, :modified_by
     remove_column :events, :modified_by
   end
 end
