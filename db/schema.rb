2fa798fd93a40fb41152cc137dc30d0c555ad023
Danny Tatom <dannytatom@gmail.com>
2012-07-25 20:48:04 -0700

    Added the DropStartsAndEndsFromEvents migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120726034601) do
+ActiveRecord::Schema.define(:version => 20120726034731) do
 
   create_table "accounts", :force => true do |t|
     t.string   "name"
@@ -42,10 +42,6 @@ ActiveRecord::Schema.define(:version => 20120726034601) do
     t.integer  "location_id"
     t.string   "type",                            :null => false
     t.string   "name",                            :null => false
-    t.date     "start_date"
-    t.time     "start_time"
-    t.date     "end_date"
-    t.time     "end_time"
     t.boolean  "remind",       :default => false
     t.datetime "created_at",                      :null => false
     t.datetime "updated_at",                      :null => false
