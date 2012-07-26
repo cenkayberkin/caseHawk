7dc065f086c65af8c8a103419740d1dd0b12fdac
Danny Tatom <dannytatom@gmail.com>
2012-07-26 12:21:51 -0700

    Removing schema info from models

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,28 +1,6 @@
-# == Schema Information
-#
-# Table name: events
-#
-#  id           :integer(4)      not null, primary key
-#  creator_id   :integer(4)      not null
-#  owner_id     :integer(4)
-#  location_id  :integer(4)
-#  type         :string(255)     not null
-#  name         :string(255)     not null
-#  remind       :boolean(1)
-#  created_at   :datetime
-#  updated_at   :datetime
-#  completed_at :datetime
-#  starts_at    :datetime
-#  ends_at      :datetime
-#  completed_by :integer(4)
-#  account_id   :integer(4)
-#  version      :integer(4)
-#  deleted_at   :datetime
-#
-
 class Event < ActiveRecord::Base
 
-  acts_as_versioned:table_name => :event_versions
+  acts_as_versioned :table_name => :event_versions
   acts_as_paranoid
   
   belongs_to :account
--- app/models/tagging.rb
+++ app/models/tagging.rb
@@ -1,17 +1,3 @@
-# == Schema Information
-#
-# Table name: taggings
-#
-#  id            :integer(4)      not null, primary key
-#  creator_id    :integer(4)
-#  tag_id        :integer(4)
-#  taggable_type :string(255)
-#  taggable_id   :integer(4)
-#  created_at    :datetime
-#  updated_at    :datetime
-#  automated     :boolean(1)      not null
-#
-
 class Tagging < ActiveRecord::Base
 
   belongs_to :taggable, :polymorphic => true
